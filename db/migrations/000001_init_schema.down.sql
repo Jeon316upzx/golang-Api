4cd550befcca2c7ba66760d51ce72465f413d166
Jeon316upzx <mamasbwoyjeon94@gmail.com>
2022-10-18 22:26:36 +0100

    fix: updated the migratedown.sql file drop table order

--- db/migrations/000001_init_schema.down.sql
+++ db/migrations/000001_init_schema.down.sql
@@ -1,6 +1,6 @@
 DROP table if exists transactions;
 DROP table if exists transfer;
-DROP table if exists owner_details;
-DROP table if exists owner;
 DROP table if exists accounts;
+DROP table if exists owner;
+DROP table if exists owner_details;
 
