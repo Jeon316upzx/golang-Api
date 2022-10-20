3951d5945c2a0e6b389511c7f41c397cae1c83e4
Jeon316upzx <mamasbwoyjeon94@gmail.com>
2022-10-20 12:06:17 +0100

    test: added util file for randomness generation for both name and amount

---
+++ db/util/random.go
@@ -0,0 +1,40 @@
+package util
+
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
+
+const alpabets = "abcdefghijklmnopqrstuvwxyz"
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func RandomInt(min, max int32) int32 {
+	return rand.Int31n(max - min + 1)
+}
+
+func RandomString(n int) string {
+	var name strings.Builder
+	l := len(alpabets)
+	for i := 0; i < n; i++ {
+		char := alpabets[rand.Intn(l)]
+		name.WriteByte(char)
+	}
+
+	return name.String()
+}
+
+func RandomOwner(n int) string {
+	return RandomString(n)
+}
+
+func RandomAmount(min, max int32) int32 {
+	return RandomInt(min, max)
+}
+
+func RandomAge(min, max int32) int32 {
+	return RandomInt(min, max)
+}
