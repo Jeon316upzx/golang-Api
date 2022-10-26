ef6ae0f7ee06b46fa51222b97d1bfdd9e163d378
Jeon316upzx <mamasbwoyjeon94@gmail.com>
2022-10-26 11:37:46 +0100

    test: updated account balance after each atomic transaction

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -46,13 +46,34 @@ func TestTransferTx(t *testing.T) {
 
 	accountID1, _ := createdAcc.LastInsertId()
 	accountID2, _ := createdAcc1.LastInsertId()
-	amount := int64(10)
+	amount := int64(130)
 	result, err := store.TransferTx(context.Background(), TransferTxParams{
 		FromAccountID: int32(accountID1),
 		ToAccountID:   int32(accountID2),
 		Amount:        amount,
 	})
 
+	owner1, _ := testQueries.GetAccountById(context.Background(), sql.NullInt32{Int32: int32(accountID1)})
+	owner2, _ := testQueries.GetAccountById(context.Background(), sql.NullInt32{Int32: int32(accountID2)})
+
+	newBal1 := owner1.Balance - amount
+	newBal2 := owner2.Balance - amount
+
+	ID1 := owner1.ID
+	ID2 := owner2.ID
+
+	ownerDetail1, _ := testQueries.UpdateAccount(context.Background(), UpdateAccountParams{
+		Balance: newBal1,
+		Owner:   sql.NullInt32{Int32: ID1},
+	})
+
+	ownerDetail2, _ := testQueries.UpdateAccount(context.Background(), UpdateAccountParams{
+		Balance: newBal2,
+		Owner:   sql.NullInt32{Int32: ID2},
+	})
+
 	require.NoError(t, err)
 	require.NotEmpty(t, result)
+	require.NotEmpty(t, ownerDetail1)
+	require.NotEmpty(t, ownerDetail2)
 }
